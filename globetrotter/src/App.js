82c0b2d20a46eba419a8bf88a683a446687e8c4d
Subhajit329 <clawdrax@gmail.com>
2025-01-24 14:06:54 +0530

    Globetrotter created

---
+++ globetrotter/src/App.js
@@ -0,0 +1,14 @@
+
+import { Navbar } from './components/navbr';
+import './Style.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ globetrotter/src/components/navbr.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <>
+        <nav className="nav-bar">
+            <div className="nav-bar">
+                <p>Hello</p>
+            </div>
+        </nav>
+        </>
+    )
+}
